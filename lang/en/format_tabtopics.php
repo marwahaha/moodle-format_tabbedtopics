3786d5aa60c44605a16725611ffb15da19555f0e
Matthias Opitz <opitz@gmx.de>
2018-10-29 15:40:51 +0000

    1st commit - renamed 'topics' to 'tabtopics'

---
+++ lang/en/format_tabtopics.php
@@ -0,0 +1,39 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_tabtopics', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_tabtopics
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add tabtopics';
+$string['currentsection'] = 'This topic';
+$string['editsection'] = 'Edit topic';
+$string['editsectionname'] = 'Edit topic name';
+$string['deletesection'] = 'Delete topic';
+$string['newsectionname'] = 'New name for topic {$a}';
+$string['sectionname'] = 'Topic';
+$string['pluginname'] = 'TabTopics format';
+$string['section0name'] = 'General';
+$string['page-course-view-tabtopics'] = 'Any course main page in tabtopics format';
+$string['page-course-view-tabtopics-x'] = 'Any course page in tabtopics format';
+$string['hidefromothers'] = 'Hide topic';
+$string['showfromothers'] = 'Show topic';
+$string['privacy:metadata'] = 'The Topics format plugin does not store any personal data.';
